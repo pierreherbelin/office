eac38297aae27e78c66901a10eabe7d938a2386d
pierre <pierreherbelin.ph@gmail.com>
2021-01-20 21:58:28 +0100

    Add a restroom with a silent zone

--- terrain.tsx
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.1" name="terrain" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
- <image source="../../Bureau/LPC_Terrain/terrain.png" width="1024" height="1024"/>
- <tile id="866">
-  <properties>
-   <property name="collides" type="bool" value="false"/>
-  </properties>
- </tile>
-</tileset>
