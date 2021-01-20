b94fcdefa2216b455907995913b4828b4d183fe7
pierre <pierreherbelin.ph@gmail.com>
2021-01-20 19:48:00 +0100

    Add some tilsets + Use a magnificient floor

---
+++ Zelda.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.1" name="Zelda" tilewidth="32" tileheight="32" tilecount="360" columns="20">
+ <image source="../../Bureau/gfx/Overworld.png" width="640" height="576"/>
+</tileset>
